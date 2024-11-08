a3adba0e115d43ecd178091a66f022ca1595eac2
Maddalideepika <deepikamaddali7@gmail.com>
2024-11-08 13:30:38 +0530

    Add files via upload

---
+++ Day 1/First.java
@@ -0,0 +1,8 @@
+import java.lang.System;
+class First
+{ 
+  public static void main(String args[])
+ {
+   System.out.println("Welcome to First Java Program");
+ }
+}
---
+++ Day 1/emp.java
@@ -0,0 +1,13 @@
+import java.lang.System;
+class emp
+{
+  int eno;
+  char ename;
+  float esal;
+  public static void main(String args[])
+  {
+   System.out.println("the given no is:"+  eno\n);
+   System.out.println("the given name is:"+  ename);
+   System.out.println("the given salary is:" + esal);
+   }
+}
